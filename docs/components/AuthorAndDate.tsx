40d1b2c14a3419278106b72ce97fdd2173e98acf
laudebugs <laudebugs@gmail.com>
2026-01-28 11:55:26 -0500

    fix date and update

--- docs/components/AuthorAndDate.tsx
+++ docs/components/AuthorAndDate.tsx
@@ -1,12 +1,12 @@
 export const AuthorAndDate = ({frontmatter}: {frontmatter: {author: string; authorLink: string; publishedOn: string}}) => {
-    const date = new Date(frontmatter.publishedOn).toLocaleDateString('en-US', {
+    const date = new Date(`${frontmatter.publishedOn}T00:00:00-05:00`).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
     });
     return (
         <div style={{marginBottom: '1.5rem'}}>
-            By <a href={frontmatter.authorLink} target="_blank" rel="noopener noreferrer">{frontmatter.author}</a> on {date}
+            by <a style={{textDecoration: 'wavy underline', textDecorationThickness: '.75px'}} href={frontmatter.authorLink} target="_blank" rel="noopener noreferrer">{frontmatter.author}</a> on {date}
         </div>
     );
 };
